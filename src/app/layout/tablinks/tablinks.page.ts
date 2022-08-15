62fb125df80ae6b530eadbf662d09453179c3d08
Daniel <ricardotavera64@gmail.com>
2022-08-15 12:17:13 -0500

    update

--- src/app/layout/tablinks/tablinks.page.ts
+++ src/app/layout/tablinks/tablinks.page.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablinksPage implements OnInit {
 
-  constructor() { }
+  username:string = 'daniels'
+
+  public appPages = [
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  constructor() {}
+
 
   ngOnInit() {
   }
