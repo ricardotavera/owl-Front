e30147464c408517d0bbda9ca3a3ac6378fa0ff7
Carlos-CodeBot <jossephrguezz@gmail.com>
2022-08-16 10:11:18 -0500

    1.1
    No funciona

--- src/app/welcome/login/login.page.ts
+++ src/app/welcome/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { LoginPageModule } from "./login.module";
 
 @Component({
   selector: 'app-login',
@@ -13,3 +15,35 @@ export class LoginPage implements OnInit {
   }
 
 }
+
+
+@Component({
+  selector: 'menu-example',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class MenuExample {
+
+constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+
+  }
+
+  openEnd(): void {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+}
+
+
+function isActive() {
+  throw new Error('Function not implemented.');
+}
+
